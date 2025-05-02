7b4b87a9d331c3397eda4e80bce2f0daa215942c
BrunoPaoloFigueroa <bruno.figueroa@usil.pe>
2025-05-02 11:53:49 -0500

    v1

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,45 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+/*
+int main(void){
+    DDRD|=0xFC;
+    DDRB|=0X0F;
+    while(1){
+PORTD|=0xFC;
+PORTB|=0X0F;
+_delay_ms(1000);
+PORTD&=~0xFC;
+PORTB&=~0X0F;
+_delay_ms(1000);
+    }
+}
+*/
+
+int main(void){
+
+    ADMUX|=(1<<REFS0)|(1<<MUX1);
+    ADCSRA|=(1<<ADEN)|(1<<ADPS2);
+    DDRD|=0xFC;
+    DDRB|=0X0F;
+    char alow;
+    char ahigh;
+
+    while(1){
+
+        ADCSRA|=(1<<ADSC);
+        while(!(ADCSRA&(1<<ADIF)));
+        alow=ADCL;
+        ahigh=ADCH;
+        ADCSRA|=(1<<ADIF);
+        PORTD&=~(0xFC);
+        PORTB&=~(0x0F);
+        PORTD|=(alow<<2);
+        PORTB|=((ahigh&0x03)<<2)|((alow>>6)&0x03);
+    }
+    return 0;
+}
+
+
 
